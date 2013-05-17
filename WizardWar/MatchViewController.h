09dbcf6f9392944f1e83f3e3c78e28b3a35fce73
Sean Clark Hess <seanhess@gmail.com>
2013-05-17 08:50:45 -0600

    broken

---
+++ WizardWar/MatchViewController.h
@@ -0,0 +1,14 @@
+//
+//  MatchViewController.h
+//  WizardWar
+//
+//  Created by Sean Hess on 5/17/13.
+//  Copyright (c) 2013 The LAB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "cocos2d.h"
+
+@interface MatchViewController : CCDirectorIOS
+
+@end
---
+++ WizardWar/MatchViewController.m
@@ -0,0 +1,147 @@
+//
+//  MatchViewController.m
+//  WizardWar
+//
+//  Created by Sean Hess on 5/17/13.
+//  Copyright (c) 2013 The LAB. All rights reserved.
+//
+
+#import "MatchViewController.h"
+#import "IntroLayer.h"
+
+@interface MatchViewController ()  <CCDirectorDelegate>
+@property (nonatomic, strong) CCDirectorIOS *director;
+@end
+
+@implementation MatchViewController
+
+- (id)init {
+    if ((self = [super init])) {
+        
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+    
+    self.navigationController.navigationBarHidden = YES;
+    
+	// CCGLView creation
+	// viewWithFrame: size of the OpenGL view. For full screen use [_window bounds]
+	//  - Possible values: any CGRect
+	// pixelFormat: Format of the render buffer. Use RGBA8 for better color precision (eg: gradients). But it takes more memory and it is slower
+	//	- Possible values: kEAGLColorFormatRGBA8, kEAGLColorFormatRGB565
+	// depthFormat: Use stencil if you plan to use CCClippingNode. Use Depth if you plan to use 3D effects, like CCCamera or CCNode#vertexZ
+	//  - Possible values: 0, GL_DEPTH_COMPONENT24_OES, GL_DEPTH24_STENCIL8_OES
+	// sharegroup: OpenGL sharegroup. Useful if you want to share the same OpenGL context between different threads
+	//  - Possible values: nil, or any valid EAGLSharegroup group
+	// multiSampling: Whether or not to enable multisampling
+	//  - Possible values: YES, NO
+	// numberOfSamples: Only valid if multisampling is enabled
+	//  - Possible values: 0 to glGetIntegerv(GL_MAX_SAMPLES_APPLE)
+	CCGLView *glView = [CCGLView viewWithFrame:[self.view bounds]
+								   pixelFormat:kEAGLColorFormatRGB565
+								   depthFormat:0
+							preserveBackbuffer:NO
+									sharegroup:nil
+								 multiSampling:NO
+							   numberOfSamples:0];
+    
+    CCDirectorIOS * director = (CCDirectorIOS*) [CCDirector sharedDirector];
+	
+	director.wantsFullScreenLayout = YES;
+	
+	// Display FSP and SPF
+	[director setDisplayStats:YES];
+	
+	// set FPS at 60
+	[director setAnimationInterval:1.0/60];
+	
+	// attach the openglView to the director
+	[director setView:glView];
+	
+	// 2D projection
+	[director setProjection:kCCDirectorProjection2D];
+	//	[director setProjection:kCCDirectorProjection3D];
+	
+	// Enables High Res mode (Retina Display) on iPhone 4 and maintains low res on all other devices
+	if( ! [director enableRetinaDisplay:YES] )
+		CCLOG(@"Retina Display Not supported");
+	
+	// Default texture format for PNG/BMP/TIFF/JPEG/GIF images
+	// It can be RGBA8888, RGBA4444, RGB5_A1, RGB565
+	// You can change this setting at any time.
+	[CCTexture2D setDefaultAlphaPixelFormat:kCCTexture2DPixelFormat_RGBA8888];
+	
+	// If the 1st suffix is not found and if fallback is enabled then fallback suffixes are going to searched. If none is found, it will try with the name without suffix.
+	// On iPad HD  : "-ipadhd", "-ipad",  "-hd"
+	// On iPad     : "-ipad", "-hd"
+	// On iPhone HD: "-hd"
+	CCFileUtils *sharedFileUtils = [CCFileUtils sharedFileUtils];
+	[sharedFileUtils setEnableFallbackSuffixes:NO];				// Default: NO. No fallback suffixes are going to be used
+	[sharedFileUtils setiPhoneRetinaDisplaySuffix:@"-hd"];		// Default on iPhone RetinaDisplay is "-hd"
+	[sharedFileUtils setiPadSuffix:@"-ipad"];					// Default on iPad is "ipad"
+	[sharedFileUtils setiPadRetinaDisplaySuffix:@"-ipadhd"];	// Default on iPad RetinaDisplay is "-ipadhd"
+	
+	// Assume that PVR images have premultiplied alpha
+	[CCTexture2D PVRImagesHavePremultipliedAlpha:YES];
+	
+	// Create a Navigation Controller with the Director
+    // I have to ADD it to it!
+
+	// for rotation and other messages
+	[director setDelegate:self];
+    self.director = director;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+    
+	[[CCDirector sharedDirector] purgeCachedData];
+}
+
+
+// The available orientations should be defined in the Info.plist file.
+// And in iOS 6+ only, you can override it in the Root View controller in the "supportedInterfaceOrientations" method.
+// Only valid for iOS 6+. NOT VALID for iOS 4 / 5.
+-(NSUInteger)supportedInterfaceOrientations {
+	
+	// iPhone only
+	if( [[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone )
+		return UIInterfaceOrientationMaskLandscape;
+	
+	// iPad only
+	return UIInterfaceOrientationMaskLandscape;
+}
+
+// Supported orientations. Customize it for your own needs
+// Only valid on iOS 4 / 5. NOT VALID for iOS 6.
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+	// iPhone only
+	if( [[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone )
+		return UIInterfaceOrientationIsLandscape(interfaceOrientation);
+	
+	// iPad only
+	// iPhone only
+	return UIInterfaceOrientationIsLandscape(interfaceOrientation);
+}
+
+// This is needed for iOS4 and iOS5 in order to ensure
+// that the 1st scene has the correct dimensions
+// This is not needed on iOS6 and could be added to the application:didFinish...
+-(void) directorDidReshapeProjection:(CCDirector*)director
+{
+	if(director.runningScene == nil) {
+		// Add the first scene to the stack. The director will draw it immediately into the framebuffer. (Animation is started automatically when the view is displayed.)
+		// and add the scene to the stack. The director will run it when it automatically when the view is displayed.
+		[director runWithScene: [IntroLayer scene]];
+	}
+}
+
+@end
