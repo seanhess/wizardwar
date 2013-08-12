a77fc086aaf315b34e205c3db8668508fc50c949
Sean Clark Hess <seanhess@gmail.com>
2013-08-12 11:07:08 -0600

    new undies

--- WizardWar/WizardSprite.m
+++ WizardWar/WizardSprite.m
@@ -369,7 +369,7 @@
     else if ([self.wizard.effect class] == [EffectUndies class]) {
         self.effect = [CCSprite spriteWithFile:@"wizard-undies.png"];
 //        self.effect.flipY = YES;
-        self.effect.position = ccp(self.wizard.direction*-15, -34);
+        self.effect.position = ccp(self.wizard.direction*-12, -30);
         
         
     }
